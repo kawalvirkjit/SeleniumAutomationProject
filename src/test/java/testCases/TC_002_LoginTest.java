eb333824427ecdea8630167c9016b8bf8eaf7ac9
kawalvirkjit <kawalvirkjit5236@gmail.com>
2022-12-10 10:54:53 -0500

    my first commit

---
+++ src/test/java/testCases/TC_002_LoginTest.java
@@ -0,0 +1,53 @@
+package testCases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+
+import pageObject.HomePage;
+import pageObject.LoginPage;
+import pageObject.MyAccountPage;
+import testBase.BaseClass;
+
+
+
+public class TC_002_LoginTest extends BaseClass
+{
+    @Test
+    public void test_Login()
+    {
+        logger.info("Starting TC_002_LoginTest");
+        
+        try
+        {                
+            HomePage hp=new HomePage(driver);
+            
+                        
+            hp.clicklogin();
+            
+            LoginPage lp=new LoginPage(driver);
+            
+            lp.setEmail(rb.getString("email")); // valid email, get it from properties file
+            
+            lp.setPassword(rb.getString("password")); // valid password, get it from properties file
+            
+            lp.clickLogin();
+            
+            MyAccountPage macc=new MyAccountPage(driver);
+            
+            boolean targetpage=macc.isMyAccountPageExists();
+                        
+            Assert.assertEquals(targetpage, true);
+            
+        }    
+        catch(Exception e)
+        {
+            Assert.fail();
+        }
+        logger.info(" Finished TC_002_LoginTest");
+        
+    }
+    
+    
+}
